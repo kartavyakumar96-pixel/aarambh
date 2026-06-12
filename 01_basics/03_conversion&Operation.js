57a1696064e9c2f313af46295a939029499cef46
kartavyakumar96-pixel <kartavyakumar96@gmail.com>
2026-06-12 05:02:45 +0000

    conversion and Operation

--- 01_basics/03_conversion&Operation.js
+++ 01_basics/03_conversion&Operation.js
@@ -1,17 +1,17 @@
-// let score = "33"
+let score = "33"
 
-// console.log(typeof score)
+console.log(typeof score)
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
 
-// //"33" => 33
-// //"33abc" => NaN (Not a number)
+//"33" => 33
+//"33abc" => NaN (Not a number)
 
-// let isLoggedIn =1
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-// console.log(booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
 
 // **************** Operations ***************** //
 
