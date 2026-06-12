8c10e4cd41df2681d107883b7658ef4407a276b9
kartavyakumar96-pixel <kartavyakumar96@gmail.com>
2026-06-12 03:45:29 +0000

    conversionof data type

---
+++ 01_basics/03_conversion&Operation.js
@@ -0,0 +1,15 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN (Not a number)
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
