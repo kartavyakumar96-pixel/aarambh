5444763b839e59d53ce6dbde5fbaeb97af801624
kartavyakumar96-pixel <kartavyakumar96@gmail.com>
2026-06-12 02:47:24 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kartavyasharma@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountPassword = "245653"
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
