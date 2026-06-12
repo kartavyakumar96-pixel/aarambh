fd57865b8f92e5da84bf5a98651c4a7104f35ba4
kartavyakumar96-pixel <kartavyakumar96@gmail.com>
2026-06-12 05:01:09 +0000

    Operations

--- 01_basics/03_conversion&Operation.js
+++ 01_basics/03_conversion&Operation.js
@@ -1,15 +1,26 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
 
-//"33" => 33
-//"33abc" => NaN (Not a number)
+// //"33" => 33
+// //"33abc" => NaN (Not a number)
 
-let isLoggedIn =1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
+// **************** Operations ***************** //
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+let GameCounter =100
+GameCounter++
+++GameCounter
+
+console.log(GameCounter)
