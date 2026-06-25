6d704ae609993c26b8d2c74ffd4d3c591fe34a7c
kartavyakumar96-pixel <kartavyakumar96@gmail.com>
2026-06-25 05:14:55 +0000

    Arrows

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,30 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(` ${this.username}, welcome to website`);
+        
+    }
+   
+}
+// user.welcomeMessage()
+
+//  function chai(){
+//     let username = "Hitesh"
+//     console.log(this.username)
+//  }
+//  chai()
+
+// const chai = ()=> {
+//     let username = "Hitesh"
+//      console.log(this)
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+// return num1+num2
+// }
+const addTwo = (num1, num2) => (num1 + num2)
+
+console.log(addTwo(3,4))
